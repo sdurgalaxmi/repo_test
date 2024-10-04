a603e209540bbbb99d985264ec2a2a4ba25e6352
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-10-04 16:26:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// index.php
+
+$host = getenv('DB_HOST') ?: 'demopostgres';
+$db   = getenv('DB_NAME') ?: 'demo2';
+$user = getenv('DB_USER') ?: 'demo2';
+$pass = getenv('DB_PASSWORD') ?: 'demo2db';
+
+$dsn = "pgsql:host=$host;dbname=$db";
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
