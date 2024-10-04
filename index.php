b95e7efed6dc57de88599fa191b5550c5ab7026f
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-10-04 16:35:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 // index.php
 
 $host = getenv('DB_HOST') ?: 'demopostgres';
-$db   = getenv('DB_NAME') ?: 'demo2';
+$db   = getenv('DB_NAME') ?: 'demodb';
 $user = getenv('DB_USER') ?: 'demo2';
-$pass = getenv('DB_PASSWORD') ?: 'demo2db';
+$pass = getenv('DB_PASSWORD') ?: 'demo2';
 
 $dsn = "pgsql:host=$host;dbname=$db";
 try {
